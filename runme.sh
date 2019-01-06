ff063cc87eebd70e3318b036015923fa7836f961
Nitin Reddy <redknitin@gmail.com>
2019-01-06 14:02:12 +0400

    Adds threads flag to Maven package (supported from Maven 3.x)

--- runme.sh
+++ runme.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #To execute with working dir set, (cd /the/dir ; /the/dir/runme.sh )
-mvn package
+
+mvn -T 4 package
+#Parallelism set to 4 threads; supported from Maven 3.x
+
 mvn exec:java -Dexec.mainClass="Repo"
 #-Dexec.args="a0 a1" for the args
